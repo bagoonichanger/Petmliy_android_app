c8040571e36b22347cc4155eb86a8ead57b1f729
bagoonichanger <bagoonichanger@gmail.com>
2022-02-09 12:35:56 +0900

    add bottomNavigationView

--- app/src/main/java/com/bagooni/petmliy_android_app/MainActivity.kt
+++ app/src/main/java/com/bagooni/petmliy_android_app/MainActivity.kt
@@ -2,10 +2,49 @@ package com.bagooni.petmliy_android_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.get
+import androidx.viewpager2.widget.ViewPager2
+import com.bagooni.petmliy_android_app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val bottomNavigationView: BottomNavigationView by lazy {
+        binding.bottomNavigationView
+    }
+
+    private val viewpager: ViewPager2 by lazy {
+        binding.viewPager
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        viewpager.adapter = viewPagerFragmentAdapter(this)
+
+        bottomNavigationView.setOnItemSelectedListener(object: NavigationBarView.OnItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when(item.itemId){
+                    R.id.home -> viewpager.currentItem = 0
+                    R.id.story -> viewpager.currentItem = 1
+                    R.id.walk -> viewpager.currentItem = 2
+                    R.id.map -> viewpager.currentItem = 3
+                }
+                return false
+            }
+        })
+
+        viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+        })
+
     }
 }
