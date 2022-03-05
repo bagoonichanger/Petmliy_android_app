08dc8b13374661b40e5f7a8bf10604aa43ad1851
bagoonichanger <bagoonichanger@gmail.com>
2022-03-05 18:54:45 +0900

    master 병합

--- app/src/main/java/com/bagooni/petmliy_android_app/MainActivity.kt
+++ app/src/main/java/com/bagooni/petmliy_android_app/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     private val walkFragment: WalkFragment by lazy {
         WalkFragment()
     }
-//확인
+// master 병합
     private val mapFragment: MapFragment by lazy {
         MapFragment()
     }
