0b8b185a31663df566c9a4e319638f4dcc240713
bagoonichanger <bagoonichanger@gmail.com>
2022-03-05 18:47:05 +0900

    commit 확인

--- app/src/main/java/com/bagooni/petmliy_android_app/MainActivity.kt
+++ app/src/main/java/com/bagooni/petmliy_android_app/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     private val walkFragment: WalkFragment by lazy {
         WalkFragment()
     }
-
+//확인
     private val mapFragment: MapFragment by lazy {
         MapFragment()
     }
