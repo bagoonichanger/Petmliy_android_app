306213807f52b4b38fc32752d47d0f36fcfebca2
eerini <yerin506@naver.com>
2022-03-11 17:07:38 +0900

    commit0311

--- app/src/main/java/com/bagooni/petmliy_android_app/home/HomeFragment.kt
+++ app/src/main/java/com/bagooni/petmliy_android_app/home/HomeFragment.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bagooni.petmliy_android_app.R
+import com.bagooni.petmliy_android_app.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+
 }
