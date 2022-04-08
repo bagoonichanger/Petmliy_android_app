590e931a085890a0c05dd9fb1069e78969fbeee9
bagoonichanger <bagoonichanger@gmail.com>
2022-04-08 09:57:26 +0900

    commit

--- app/src/main/java/com/bagooni/petmliy_android_app/walk/WalkFragment.kt
+++ app/src/main/java/com/bagooni/petmliy_android_app/walk/WalkFragment.kt
@@ -15,11 +15,7 @@ class WalkFragment : Fragment() { // 시작
     private var _binding: FragmentWalkBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentWalkBinding.inflate(inflater, container, false)
         return binding.root
     }
