8ae3cef1ddf394013c080719bb6e6ffb68505f41
eerini <yerin506@naver.com>
2022-04-11 13:07:01 +0900

    walk layout

--- app/src/main/java/com/bagooni/petmliy_android_app/walk/WalkFragment.kt
+++ app/src/main/java/com/bagooni/petmliy_android_app/walk/WalkFragment.kt
@@ -1,19 +1,30 @@
 package com.bagooni.petmliy_android_app.walk
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bagooni.petmliy_android_app.R
+import com.bagooni.petmliy_android_app.databinding.FragmentWalkBinding
 
+@Suppress("UNREACHABLE_CODE")
 class WalkFragment : Fragment() { // 시작
+    private var _binding: FragmentWalkBinding?=null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_walk, container, false)
+        _binding = FragmentWalkBinding.inflate(inflater,container,false)
+        return binding.root
+
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+
+        }
     }
 }
+
 
