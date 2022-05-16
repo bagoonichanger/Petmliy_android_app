d00ae5a6487e5eea7c4947e39c3670858386c611
bagoonichanger <bagoonichanger@gmail.com>
2022-05-16 10:54:56 +0900

    저장한 장소 delete 추가

--- app/src/main/java/com/bagooni/petmliy_android_app/map/model/Dto/LikePlaceDto.kt
+++ app/src/main/java/com/bagooni/petmliy_android_app/map/model/Dto/LikePlaceDto.kt
@@ -3,10 +3,11 @@ package com.bagooni.petmliy_android_app.map.model.Dto
 import com.google.gson.annotations.SerializedName
 
 data class LikePlaceDto(
-    @SerializedName("id") var id: Int?,
+    @SerializedName("placeId") var placeId: Int?,
     @SerializedName("placeName") var placeName: String?,
     @SerializedName("phone") var phone: String?,
     @SerializedName("address") var address: String?,
     @SerializedName("url") var url: String?,
-    @SerializedName("categories") var categories: String?
+    @SerializedName("categories") var categories: String?,
+    @SerializedName("userImg") var userImg: String?
 )
