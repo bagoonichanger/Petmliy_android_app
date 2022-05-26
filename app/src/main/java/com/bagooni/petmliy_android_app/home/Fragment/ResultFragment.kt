ca2b27bed50ad4d4a567e4635c452bc3b316196b
eerini <yerin506@naver.com>
2022-05-27 01:51:46 +0900

    post_update

--- app/src/main/java/com/bagooni/petmliy_android_app/home/Fragment/ResultFragment.kt
+++ app/src/main/java/com/bagooni/petmliy_android_app/home/Fragment/ResultFragment.kt
@@ -8,10 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.res.Resources
 import android.database.Cursor
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.ImageDecoder
-import android.graphics.Rect
+import android.graphics.*
 import android.net.Uri
 import android.os.*
 import android.provider.MediaStore
